356ff5acae69c18880f35dd0d39e05c0be565042
mattias-arbin <mattias.arbin@ikama.se>
2019-08-14 18:33:27 +0200

    Adding exit command to shell

--- bauta-core/src/main/java/ikama/bauta/cli/CommandShell.java
+++ bauta-core/src/main/java/ikama/bauta/cli/CommandShell.java
@@ -60,8 +60,8 @@ public class CommandShell {
     }
 
     @ShellMethod("Exit")
-    public String exit(String jobName) {
-        
+    public void exit(String jobName) {
+
         System.exit(0);
     }
 }
