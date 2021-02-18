a4223b77db796b888e042dd9b876597d7c560435
maar16 <mattias.arbin@teliacompany.com>
2021-02-18 21:53:42 +0100

    Fixing dependency issues

--- bauta-core/src/main/java/se/ikama/bauta/scheduling/JobTrigger.java
+++ bauta-core/src/main/java/se/ikama/bauta/scheduling/JobTrigger.java
@@ -1,19 +1,10 @@
 package se.ikama.bauta.scheduling;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
 import lombok.Data;
 
-@Entity
-@Table(name = "SCHEDULING_JOB_TRIGGER")
 @Data
 public class JobTrigger {
     public enum TriggerType {CRON, JOB_COMPLETION}
-    @Id()
-    @GeneratedValue()
     Long id;
     
     TriggerType triggerType;
--- bauta-core/src/main/java/se/ikama/bauta/scheduling/SchedulingConfigurationRepository.java
+++
@@ -1,7 +0,0 @@
-package se.ikama.bauta.scheduling;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface SchedulingConfigurationRepository extends CrudRepository<SchedulingConfiguration, String> {
-
-}
