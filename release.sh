455b959532e5f59d94ed909db9ec379346923482
Axel <axelb@ikama.se>
2021-07-26 12:39:08 +0200

    first draft of release script

---
+++ release.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+
+while getopts v:d:p flag
+do
+  # shellcheck disable=SC2220
+  case "${flag}" in
+    v) releaseVersion=${OPTARG};;
+    d) developmentVersion=${OPTARG};;
+    p) password=${OPTARG};;
+  esac
+done
+
+#ta bort SNAPSHOT taggen
+git pull
+sed -i "s/${releaseVersion}-SNAPSHOT/${releaseVersion}/g" pom.xml
+cd bauta-autoconfigure
+sed -i "s/${releaseVersion}-SNAPSHOT/${releaseVersion}/g" pom.xml
+cd ..
+cd bauta-core
+sed -i "s/${releaseVersion}-SNAPSHOT/${releaseVersion}/g" pom.xml
+cd ..
+cd bauta-sample
+sed -i "s/${releaseVersion}-SNAPSHOT/${releaseVersion}/g" pom.xml
+cd ..
+cd bauta-sample-oracle
+sed -i "s/${releaseVersion}-SNAPSHOT/${releaseVersion}/g" pom.xml
+cd ..
+cd bauta-sample-php
+sed -i "s/${releaseVersion}-SNAPSHOT/${releaseVersion}/g" pom.xml
+cd ..
+cd bauta-starter
+sed -i "s/${releaseVersion}-SNAPSHOT/${releaseVersion}/g" pom.xml
+cd ..
+
+git add . 
+git commit -m "removed snapshot"
+git push
+
+#deploy maven 
+mvn clean deploy-X-Dgpg.passphrase=${password} -P ossrh,release,production
+
+#create tag and release on github
+git tag -a "v${releaseVersion}" -m "Version ${releaseVersion}"
+git push origin "v${releaseVersion}"
+
+
+sed -i "s/${releaseVersion}/${developmentVersion}-SNAPSHOT/g" pom.xml
+cd bauta-autoconfigure
+sed -i "s/${releaseVersion}/${developmentVersion}-SNAPSHOT/g" pom.xml
+cd ..
+cd bauta-core
+sed -i "s/${releaseVersion}/${developmentVersion}-SNAPSHOT/g" pom.xml
+cd ..
+cd bauta-sample
+sed -i "s/${releaseVersion}/${developmentVersion}-SNAPSHOT/g" pom.xml
+cd .. 
+cd bauta-sample-oracle
+sed -i "s/${releaseVersion}/${developmentVersion}-SNAPSHOT/g" pom.xml
+cd ..
+cd bauta-sample-php
+sed -i "s/${releaseVersion}/${developmentVersion}-SNAPSHOT/g" pom.xml
+cd ..
+cd bauta-starter
+sed -i "s/${releaseVersion}/${developmentVersion}-SNAPSHOT/g" pom.xml
+cd ..
+
+git add .
+git commit -m "Bumped version"
+git push
