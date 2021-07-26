58755fd81575f8c6bc930dc6db0fde8c6c5394bd
Axel <axelb@ikama.se>
2021-07-26 13:32:47 +0200

    added settings file

--- release.sh
+++ release.sh
@@ -11,62 +11,60 @@ do
   esac
 done
 
-ls -la
+#ta bort SNAPSHOT taggen
+git pull
+sed -i "s/${releaseVersion}-SNAPSHOT/${releaseVersion}/g" pom.xml
+cd bauta-autoconfigure
+sed -i "s/${releaseVersion}-SNAPSHOT/${releaseVersion}/g" pom.xml
+cd ..
+cd bauta-core
+sed -i "s/${releaseVersion}-SNAPSHOT/${releaseVersion}/g" pom.xml
+cd ..
+cd bauta-sample
+sed -i "s/${releaseVersion}-SNAPSHOT/${releaseVersion}/g" pom.xml
+cd ..
+cd bauta-sample-oracle
+sed -i "s/${releaseVersion}-SNAPSHOT/${releaseVersion}/g" pom.xml
+cd ..
+cd bauta-sample-php
+sed -i "s/${releaseVersion}-SNAPSHOT/${releaseVersion}/g" pom.xml
+cd ..
+cd bauta-starter
+sed -i "s/${releaseVersion}-SNAPSHOT/${releaseVersion}/g" pom.xml
+cd ..
 
-# #ta bort SNAPSHOT taggen
-# git pull
-# sed -i "s/${releaseVersion}-SNAPSHOT/${releaseVersion}/g" pom.xml
-# cd bauta-autoconfigure
-# sed -i "s/${releaseVersion}-SNAPSHOT/${releaseVersion}/g" pom.xml
-# cd ..
-# cd bauta-core
-# sed -i "s/${releaseVersion}-SNAPSHOT/${releaseVersion}/g" pom.xml
-# cd ..
-# cd bauta-sample
-# sed -i "s/${releaseVersion}-SNAPSHOT/${releaseVersion}/g" pom.xml
-# cd ..
-# cd bauta-sample-oracle
-# sed -i "s/${releaseVersion}-SNAPSHOT/${releaseVersion}/g" pom.xml
-# cd ..
-# cd bauta-sample-php
-# sed -i "s/${releaseVersion}-SNAPSHOT/${releaseVersion}/g" pom.xml
-# cd ..
-# cd bauta-starter
-# sed -i "s/${releaseVersion}-SNAPSHOT/${releaseVersion}/g" pom.xml
-# cd ..
+git add . 
+git commit -m "removed snapshot"
+git push
 
-# git add . 
-# git commit -m "removed snapshot"
-# git push
+#deploy maven 
+mvn -s settings.xml clean deploy -X -Dgpg.passphrase=${password} -P ossrh,release,production -f pom.xml
 
-# #deploy maven 
-# mvn clean deploy -X -Dgpg.passphrase=${password} -P ossrh,release,production -f pom.xml
-
-# #create tag and release on github
-# git tag -a "v${releaseVersion}" -m "Version ${releaseVersion}"
-# git push origin "v${releaseVersion}"
+#create tag and release on github
+git tag -a "v${releaseVersion}" -m "Version ${releaseVersion}"
+git push origin "v${releaseVersion}"
 
 
-# sed -i "s/${releaseVersion}/${developmentVersion}-SNAPSHOT/g" pom.xml
-# cd bauta-autoconfigure
-# sed -i "s/${releaseVersion}/${developmentVersion}-SNAPSHOT/g" pom.xml
-# cd ..
-# cd bauta-core
-# sed -i "s/${releaseVersion}/${developmentVersion}-SNAPSHOT/g" pom.xml
-# cd ..
-# cd bauta-sample
-# sed -i "s/${releaseVersion}/${developmentVersion}-SNAPSHOT/g" pom.xml
-# cd .. 
-# cd bauta-sample-oracle
-# sed -i "s/${releaseVersion}/${developmentVersion}-SNAPSHOT/g" pom.xml
-# cd ..
-# cd bauta-sample-php
-# sed -i "s/${releaseVersion}/${developmentVersion}-SNAPSHOT/g" pom.xml
-# cd ..
-# cd bauta-starter
-# sed -i "s/${releaseVersion}/${developmentVersion}-SNAPSHOT/g" pom.xml
-# cd ..
+sed -i "s/${releaseVersion}/${developmentVersion}-SNAPSHOT/g" pom.xml
+cd bauta-autoconfigure
+sed -i "s/${releaseVersion}/${developmentVersion}-SNAPSHOT/g" pom.xml
+cd ..
+cd bauta-core
+sed -i "s/${releaseVersion}/${developmentVersion}-SNAPSHOT/g" pom.xml
+cd ..
+cd bauta-sample
+sed -i "s/${releaseVersion}/${developmentVersion}-SNAPSHOT/g" pom.xml
+cd .. 
+cd bauta-sample-oracle
+sed -i "s/${releaseVersion}/${developmentVersion}-SNAPSHOT/g" pom.xml
+cd ..
+cd bauta-sample-php
+sed -i "s/${releaseVersion}/${developmentVersion}-SNAPSHOT/g" pom.xml
+cd ..
+cd bauta-starter
+sed -i "s/${releaseVersion}/${developmentVersion}-SNAPSHOT/g" pom.xml
+cd ..
 
-# git add .
-# git commit -m "Bumped version"
-# git push
+git add .
+git commit -m "Bumped version"
+git push
