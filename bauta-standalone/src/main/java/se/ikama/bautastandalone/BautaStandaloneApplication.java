1777960bc382ea0362c3981937f8d6c8c01d1e86
lukas rasmussen <lukasr@ikama.se>
2021-07-28 09:33:25 +0200

    Bauta-standalone module created

---
+++ bauta-standalone/src/main/java/se/ikama/bautastandalone/BautaStandaloneApplication.java
@@ -0,0 +1,13 @@
+package se.ikama.bautastandalone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BautaStandaloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BautaStandaloneApplication.class, args);
+    }
+
+}
---
+++ bauta-standalone/src/test/java/se/ikama/bautastandalone/BautaStandaloneApplicationTests.java
@@ -0,0 +1,13 @@
+package se.ikama.bautastandalone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BautaStandaloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
