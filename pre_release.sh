d74c8257c7ea668e5aef283fb04911e70195795c
mattias-arbin <mattias.arbin@ikama.se>
2021-08-04 11:37:07 +0200

    Release workflow rework

---
+++ pre_release.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+while getopts v:d:p flag
+do
+  # shellcheck disable=SC2220
+  case "${flag}" in
+    v) releaseVersion=${OPTARG};;
+    d) developmentVersion=${OPTARG};;
+    p) password=${OPTARG};;
+  esac
+done
+
+#ta bort SNAPSHOT taggen
+sed -i "s/${releaseVersion}-SNAPSHOT/${releaseVersion}/g" pom.xml
+cd bauta-autoconfigure
+sed -i "s/${releaseVersion}-SNAPSHOT/${releaseVersion}/g" pom.xml
+cd ..
+cd bauta-core
+sed -i "s/${releaseVersion}-SNAPSHOT/${releaseVersion}/g" pom.xml
+cd ..
+cd bauta-sample
+sed -i "s/${releaseVersion}-SNAPSHOT/${releaseVersion}/g" pom.xml
+cd ..
+cd bauta-sample-oracle
+sed -i "s/${releaseVersion}-SNAPSHOT/${releaseVersion}/g" pom.xml
+cd ..
+cd bauta-sample-php
+sed -i "s/${releaseVersion}-SNAPSHOT/${releaseVersion}/g" pom.xml
+cd ..
+cd bauta-starter
+sed -i "s/${releaseVersion}-SNAPSHOT/${releaseVersion}/g" pom.xml
+cd ..
+
