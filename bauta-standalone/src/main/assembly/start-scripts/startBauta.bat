1019a503c2ffd409395e6fe668dec88b38fd1758
mattias-arbin <mattias.arbin@ikama.se>
2021-09-03 16:33:55 +0200

    Fixing windows version

--- bauta-standalone/src/main/assembly/start-scripts/startBauta.bat
+++ bauta-standalone/src/main/assembly/start-scripts/startBauta.bat
@@ -1,33 +1,5 @@
 @echo off
 title Starting Bauta 
-SET SCRIPTPATH=%~dp0
-echo %SCRIPTPATH:~0,-1%
-if exist %1\ ( 
-  echo Directory %1 exists.
-) else (
-  mkdir %1
-  echo Created directory %1 
-)
-
-if exist %1\jobs\ (
-  echo Directory %1\jobs exists.
-) else (
-  cd %1
-  mkdir jobs
-  echo Created directory jobs
-  cd ..
-)
-
-if exist %1\logs\ (
-  echo Directory %1\logs exists.
-) else (
-  cd %1
-  mkdir logs
-  echo Created directory logs
-  cd ..
-)
-
-SET CURRENTDIR=%cd%
-cd %SCRIPTPATH:~0,-1%
-echo Running command: java -Dbauta.homeDir=%~f1\ -Dbauta.jobBeansDir=%~f1\jobs -Dbauta.logDir=%~f1\logs -Dspring.profiles.active=demo -jar bauta-sample-0.0.60-SNAPSHOT.jar
-java -Dbauta.homeDir=%~f1\ -Dbauta.jobBeansDir=%~f1\jobs -Dbauta.logDir=%~f1\logs -Dspring.profiles.active=demo -jar bauta-sample-0.0.60-SNAPSHOT.jar
+SET BAUTA_HOME=%~dp0..\home
+java -Dbauta.homeDir=%BAUTA_HOME% -Dspring.profiles.active=dev -jar bauta-standalone.jar
+pause
