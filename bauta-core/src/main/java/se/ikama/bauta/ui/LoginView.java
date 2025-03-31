befd8ba80c0d362261040c328ad557aacaa3a760
Mattias Arbin <mattiasa@ikama.se>
2025-03-31 11:32:39 +0200

    Improved login view

--- bauta-core/src/main/java/se/ikama/bauta/ui/LoginView.java
+++ bauta-core/src/main/java/se/ikama/bauta/ui/LoginView.java
@@ -1,10 +1,14 @@
 package se.ikama.bauta.ui;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+
 import com.vaadin.flow.component.AttachEvent;
 import com.vaadin.flow.component.html.Anchor;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment;
 import com.vaadin.flow.router.*;
@@ -13,6 +17,7 @@ import com.vaadin.flow.server.auth.AnonymousAllowed;
 import com.vaadin.flow.theme.lumo.Lumo;
 
 import lombok.extern.slf4j.Slf4j;
+import se.ikama.bauta.core.BautaManager;
 import se.ikama.bauta.security.SecurityUtils;
 
 @Route("login")
@@ -21,10 +26,20 @@ import se.ikama.bauta.security.SecurityUtils;
 @Slf4j
 public class LoginView extends Div implements BeforeEnterObserver {
 
-    boolean loggedOut = false;
-
-    Paragraph loggedOutMessage;
     
+    @Value("${bauta.application.title}")
+	private String applicationTitle;
+
+    private boolean loggedOut = false;
+
+    private Paragraph loggedOutMessage = new Paragraph();
+    private Span serverInfo = new Span();
+    private Paragraph welcomeMessage = new Paragraph();
+
+    @Autowired
+	BautaManager bautaManager;
+
+
 
     // URL that Spring Security uses to connect to Google services
     private static final String OAUTH_URL = "/oauth2/authorization/keycloak";
@@ -38,6 +53,10 @@ public class LoginView extends Div implements BeforeEnterObserver {
     @Override
 	protected void onAttach(AttachEvent attachEvent) {
 		setTheme(true);
+        serverInfo.setText(bautaManager.getShortServerInfo());
+        welcomeMessage.setText("Welcome to " + applicationTitle);
+        
+
     }
 
 
@@ -46,10 +65,11 @@ public class LoginView extends Div implements BeforeEnterObserver {
         var loginOverlay = new VerticalLayout();
         loginOverlay.setMargin(false);
         loginOverlay.setPadding(false);
+        loginOverlay.setSpacing(false);
         loginOverlay.setMaxWidth("300px");
         loginOverlay.setClassName("login-overlay");
-        Image img = new Image(new StreamResource("myimage.png",
-				() -> getClass().getResourceAsStream("/static/images/bauta-logo-light.png")), "Bauta logo");
+        Image img = new Image(new StreamResource("bauta_splash.jpg",
+				() -> getClass().getResourceAsStream("/static/images/bauta_splash.jpg")), "Bauta splash");
         
 		// Header
         var header = new Div();
@@ -57,13 +77,15 @@ public class LoginView extends Div implements BeforeEnterObserver {
         header.add(img);
         loginOverlay.add(header);
         // Body
-        var body = new VerticalLayout();
-        body.setAlignItems(Alignment.CENTER);
+        var body = new Div();
+        body.setClassName("login-body");
+        serverInfo.setClassName("server-info");
+        body.add(serverInfo);
         loggedOutMessage = new Paragraph("You have been logged out.");
         loggedOutMessage.setVisible(false);
         body.add(loggedOutMessage);
+        body.add(welcomeMessage);
         
-        body.add(new Paragraph("Welcome to Bauta!"));
 
         // Navigate to OATH URL
         Anchor loginLink = new Anchor(OAUTH_URL, "Login with IDP");
@@ -82,8 +104,8 @@ public class LoginView extends Div implements BeforeEnterObserver {
     @Override
     public void beforeEnter(BeforeEnterEvent event) {
         if (SecurityUtils.isUserLoggedIn()) {
-            log.debug("User is logged in. Redirecting to /");
-            event.forwardTo("/");
+            //log.debug("User is logged in. Redirecting to /");
+            //event.forwardTo("/");
             return;
         }
         var logoutPresent = event.getLocation().getQueryParameters().getSingleParameter("logout");
