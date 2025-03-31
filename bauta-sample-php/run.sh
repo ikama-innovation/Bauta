d6e2648f0000cf79175b87e6818ec0b0d4a24c38
Mattias Arbin <mattiasa@ikama.se>
2025-03-31 11:45:24 +0200

    POM version 2.0.4-SNAPSHOT

--- bauta-sample-php/run.sh
+++ bauta-sample-php/run.sh
@@ -1,2 +1,2 @@
 # Start server with a spring profile. The profile must be defined in src/main/resources/application.yml
-java -Dspring.profiles.active=mattias,productionMode -jar target/bauta-sample-php-0.0.62-SNAPSHOT.jar
+java -Dspring.profiles.active=mattias,productionMode -jar target/bauta-sample-php.jar
