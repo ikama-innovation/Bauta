b1a757bc69cb780efd44e89e258c155bb24816d8
Mattias Arbin <mattiasa@ikama.se>
2025-04-24 14:00:08 +0200

    Changing push transport from websockets to long polling to avoid issues with websockets

--- bauta-core/src/main/java/se/ikama/bauta/BautaApplication.java
+++ bauta-core/src/main/java/se/ikama/bauta/BautaApplication.java
@@ -9,13 +9,14 @@ import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.component.page.Viewport;
 import com.vaadin.flow.shared.communication.PushMode;
+import com.vaadin.flow.shared.ui.Transport;
 import com.vaadin.flow.theme.Theme;
 import com.vaadin.flow.theme.lumo.Lumo;
 
 @ComponentScan(basePackages = "se.ikama.bauta")
 @SpringBootApplication(exclude = BatchAutoConfiguration.class)
 @Theme(value = "bauta", variant = Lumo.DARK)
-@Push(value = PushMode.AUTOMATIC)
+@Push(value = PushMode.AUTOMATIC, transport = Transport.LONG_POLLING)
 @Viewport("width=device-width, minimum-scale=1, initial-scale=1,user-scalable=yes, viewport-fit=cover")
 public class BautaApplication implements AppShellConfigurator {
     public static void main(String[] args) {
