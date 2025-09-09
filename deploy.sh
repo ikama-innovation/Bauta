2b8bf485013909c9ad64ea74751701e8bafee9f5
Mattias Arbin <mattiasa@ikama.se>
2025-09-09 21:51:32 +0200

    Maven Central build/deploy

--- deploy.sh
+++
@@ -1,2 +0,0 @@
-# gpg.passphrase must be set in your settings.xml
-mvn clean deploy  -Possrh,release,production -f pom.xml
--- release.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#deploy maven 
-mvn -s settings.xml clean deploy -P release,production -f pom.xml
-
-
