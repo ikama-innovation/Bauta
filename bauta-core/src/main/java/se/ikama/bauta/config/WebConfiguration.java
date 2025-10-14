786273afcf13470416f13bb62c33a3144a06b272
Mattias Arbin <mattias.arbin@teliacompany.com>
2025-10-14 16:55:53 +0200

    Trying a workaround for https://github.com/spring-projects/spring-security/issues/5535

--- bauta-core/src/main/java/se/ikama/bauta/config/WebConfiguration.java
+++ bauta-core/src/main/java/se/ikama/bauta/config/WebConfiguration.java
@@ -4,9 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.web.filter.ForwardedHeaderFilter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -43,7 +45,13 @@ public class WebConfiguration implements WebMvcConfigurer {
 	 * @return
 	 */
 	@Bean
-    public ForwardedHeaderFilter forwardedHeaderFilter() {
-        return new ForwardedHeaderFilter();
-    }
+	FilterRegistrationBean<ForwardedHeaderFilter> forwardedHeaderFilter() {
+		
+	    final FilterRegistrationBean<ForwardedHeaderFilter> filterRegistrationBean = new FilterRegistrationBean<ForwardedHeaderFilter>();
+	    
+	    filterRegistrationBean.setFilter(new ForwardedHeaderFilter());
+	    filterRegistrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+	    
+	    return filterRegistrationBean;
+	}
 }
